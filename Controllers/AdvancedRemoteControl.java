261d992a6d3a83a72c806dc47a10c2cdc4658c56
Pedro Paulo <pedropaulo062011@gmail.com>
2026-04-28 10:14:59 -0300

    :sparkles: Adição de nova abstração para demonstrar extensão

---
+++ Controllers/AdvancedRemoteControl.java
@@ -0,0 +1,14 @@
+package Controllers;
+
+import Devices.Device;
+
+public class AdvancedRemoteControl extends RemoteControl {
+    public AdvancedRemoteControl(Device device) {
+        super(device);
+    }
+
+    public void mute() {
+        System.out.println("Controle Avançado: Pressionou Mute.");
+        device.setVolume(0);
+    }
+}
